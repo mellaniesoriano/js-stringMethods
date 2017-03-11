d4c6c7ce6ff3b19468f4a1e3ca101decfcc43cb9
mellaniesoriano <msoriano808@gmail.com>
2017-03-10 19:49:30 -1000

    finished ex 5

--- exercise.js
+++ exercise.js
@@ -41,6 +41,8 @@ console.log(worldLocator);
 /*4. Declare a new variable named `scissorHand` that will generate a new string "are afraid" from the existing string below. Console.log your result.*/
 
 var edward = "People are afraid of me because I am different";
+var scissorHand = edward.slice(7,17);
+console.log(scissorHand);
 
 
 
@@ -50,6 +52,8 @@ var edward = "People are afraid of me because I am different";
 
 var grasshopper = "In order to taste my cup of water you must first fill your cup.";
 
+var theSifu = grasshopper.replace("fill", "empty");
+console.log(theSifu);
 
 
 /*The toLowerCase() method converts the characters in a string to lower case.*/
